c51875a2b1d8e69d58497d53468d6d7b952eed37
Phil Hawksworth <phil@netlify.com>
2021-09-09 17:08:42 +0100

    use page data

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,19 @@
 <template>
-   <div>
-    <h1>Menu</h1>
-    <ul>
-      <li v-for="item of menu" :key=item.title>
-        <h2>{{ item.title }}</h2>
-        <div>{{ item.description}}</div>
-        <p>{{ item.currency}}{{ item.price}}</p>
-      </li>
-    </ul>
-     </div> 
+  <div>
+    <h1>{{ page.title }}</h1>
+    <div>{{ page.body }}</div>
+    <p>
+      <a href="/menu">Look at the menu</a>
+    </p>
+  </div> 
 </template>
 
 <script>
   export default {
     data() {
       return {
-        menu: require("../data/menu.json")
+        menu: require("../data/menu.json"),
+        page : require("../data/pages.json").home
       }
     }
   }
---
+++ pages/menu.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+    <h1>{{ page.title }}</h1>
+    <div>{{ page.body }}</div>
+    <p>
+      <a href="/">Look at the home page</a>
+    </p>
+    <h2>Menu</h2>
+    <ul>
+      <li v-for="item of menu" :key=item.title>
+        <h2>{{ item.title }}</h2>
+        <div>{{ item.description}}</div>
+        <p>{{ item.currency}}{{ item.price}}</p>
+      </li>
+    </ul>
+     </div> 
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        menu: require("../data/menu.json"),
+        page : require("../data/pages.json").menu
+      }
+    }
+  }
+</script>
